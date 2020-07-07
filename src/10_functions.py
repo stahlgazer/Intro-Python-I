99bb274590a1984b611593cb3e613d04ef76d123
Gavin S <stahlgavin@gmail.com>
2020-07-06 22:28:06 -0700

    tuples + functions

--- src/10_functions.py
+++ src/10_functions.py
@@ -1,12 +1,22 @@
 # Write a function is_even that will return true if the passed-in number is even.
 
 # YOUR CODE HERE
+def is_even(number):
+    if (number % 2) == 0:
+        print('true')
+    else:
+        print('false')
+
+is_even(12)
+is_even(3)
 
 # Read a number from the keyboard
 num = input("Enter a number: ")
 num = int(num)
 
 # Print out "Even!" if the number is even. Otherwise print "Odd"
-
 # YOUR CODE HERE
-
+if (num % 2) == 0:
+    print('Even!')
+else:
+    print('Odd')
